75b776280d6cc1b660c7ee0bc0dc466f6510eb81
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-07-30 16:06:43 +0900

    Rubocopによる修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,12 +24,12 @@ RSpec.describe User, type: :model do
       post_user = FactoryBot.create(:user)
       @user.email = post_user.email
       @user.valid?
-      expect(@user.errors.full_messages).to include("Email has already been taken")
+      expect(@user.errors.full_messages).to include('Email has already been taken')
     end
     it 'メールアドレスは@を含まないと登録できない' do
       @user.email = 'testmail'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Email is invalid")
+      expect(@user.errors.full_messages).to include('Email is invalid')
     end
     it 'パスワードが空だと登録できない' do
       @user.password = ''
@@ -40,13 +40,13 @@ RSpec.describe User, type: :model do
       @user.password = 'abcd1'
       @user.password_confirmation = 'abcd1'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
     it 'パスワードは英数字混合でないと登録できない' do
       @user.password = 'abcdef'
       @user.password_confirmation = 'abcdef'
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password should be in mixed alphanumeric characters")
+      expect(@user.errors.full_messages).to include('Password should be in mixed alphanumeric characters')
     end
     it 'パスワードとパスワード(確認)が一致しないと登録できない' do
       @user.password = 'abcdef1'
@@ -71,7 +71,8 @@ RSpec.describe User, type: :model do
       @user.last_name = Faker::Name.name
       @user.first_name = Faker::Name.name
       @user.valid?
-      expect(@user.errors.full_messages).to include("First name should be a full-width name", "Last name should be a full-width name")
+      expect(@user.errors.full_messages).to include('First name should be a full-width name',
+                                                    'Last name should be a full-width name')
     end
     it 'お名前カナ(全角)は名字が空だと登録できない' do
       @user.last_kananame = ''
@@ -87,7 +88,8 @@ RSpec.describe User, type: :model do
       @user.last_kananame = Faker::Name.name
       @user.first_kananame = Faker::Name.name
       @user.valid?
-      expect(@user.errors.full_messages).to include("First kananame should be a full-width katakana name", "Last kananame should be a full-width katakana name")
+      expect(@user.errors.full_messages).to include('First kananame should be a full-width katakana name',
+                                                    'Last kananame should be a full-width katakana name')
     end
     it '生年月日が空だと登録できない' do
       @user.birthday = ''
