85c401a4cab57716af8bfa51858eb1d44146a80c
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-07-31 16:49:39 +0900

    正規表現の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ApplicationRecord
   validates :password, presence: true,
                        format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: 'should be in mixed alphanumeric characters' }
   validates :first_name, presence: true,
-                         format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[一-龠々])+\z/, message: 'should be a full-width name' }
+                         format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'should be a full-width name' }
   validates :last_name, presence: true,
-                        format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[一-龠々])+\z/, message: 'should be a full-width name' }
+                        format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'should be a full-width name' }
   validates :first_kananame, presence: true,
-                             format: { with: /\A(?:\p{Katakana})+\z/, message: 'should be a full-width katakana name' }
+                             format: { with: /\A[ァ-ヶ]+\z/, message: 'should be a full-width katakana name' }
   validates :last_kananame, presence: true,
-                            format: { with: /\A(?:\p{Katakana})+\z/, message: 'should be a full-width katakana name' }
+                            format: { with: /\A[ァ-ヶ]+\z/, message: 'should be a full-width katakana name' }
   validates :birthday, presence: true
 end
