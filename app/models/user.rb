2dc37b2b7281c8d6edd52ffc2be44e916fc55870
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-07-31 18:32:59 +0900

    パスワードのvalidation修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password, presence: true,
-                       format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: 'should be in mixed alphanumeric characters' }
+  validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: 'should be in mixed alphanumeric characters' }
   validates :first_name, presence: true,
                          format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'should be a full-width name' }
   validates :last_name, presence: true,
