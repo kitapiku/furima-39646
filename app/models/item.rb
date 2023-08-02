6fbb8ff8256b8aa17d0b3956b0e657a70dc685a2
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-08-02 17:44:55 +0900

    商品価格は半角の数字のみを保存するバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,9 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price , presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price , presence: true,
+                     numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
+                     format: { with: /\A\d+\z/, message: "should be half-width numbers" }
   validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :schedule_id, numericality: { other_than: 1, message: "can't be blank" }
