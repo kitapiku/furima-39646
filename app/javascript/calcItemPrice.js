145530b39ee292dc5ca689c9bba7c2cc82c0d2b6
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-08-02 18:52:48 +0900

    出品機能実装完了(仮1)

--- app/javascript/calcItemPrice.js
+++ app/javascript/calcItemPrice.js
@@ -19,5 +19,43 @@ function calcItemPrice() {
     }
   });
 };
+function calcItemPriceForRender() {
+  const priceInput = document.getElementById("item-price");
+  const itemPrice = parseFloat(priceInput.value);
+  if (isNaN(itemPrice)) {
+    const addTaxPrice = document.getElementById("add-tax-price");
+    const addProfit = document.getElementById("profit");
+    addTaxPrice.innerHTML = "";
+    addProfit.innerHTML = "";
+  } else {
+    let taxPrice = itemPrice * 0.1;
+    taxPrice = Math.floor(taxPrice);
+    let profit = itemPrice - taxPrice;
+    profit = Math.floor(profit);
+    const addTaxPrice = document.getElementById("add-tax-price");
+    const addProfit = document.getElementById("profit");
+    addTaxPrice.innerHTML = `${taxPrice}`;
+    addProfit.innerHTML = `${profit}`;
+  }
+  priceInput.addEventListener("keyup", () => {
+    const itemPrice = parseFloat(priceInput.value);
+    if (isNaN(itemPrice)) {
+      const addTaxPrice = document.getElementById("add-tax-price");
+      const addProfit = document.getElementById("profit");
+      addTaxPrice.innerHTML = "";
+      addProfit.innerHTML = "";
+    } else {
+      let taxPrice = itemPrice * 0.1;
+      taxPrice = Math.floor(taxPrice);
+      let profit = itemPrice - taxPrice;
+      profit = Math.floor(profit);
+      const addTaxPrice = document.getElementById("add-tax-price");
+      const addProfit = document.getElementById("profit");
+      addTaxPrice.innerHTML = `${taxPrice}`;
+      addProfit.innerHTML = `${profit}`;
+    }
+  });
+};
 
 window.addEventListener('turbo:load', calcItemPrice);
+window.addEventListener('turbo:render', calcItemPriceForRender);
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,12 +8,12 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_status
   belongs_to :schedule
 
+  validates :image, presence: true
   validates :name, presence: true
   validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price , presence: true,
-                     numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
-                     format: { with: /\A\d+\z/, message: "should be half-width numbers" }
+                     numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
   validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :schedule_id, numericality: { other_than: 1, message: "can't be blank" }
