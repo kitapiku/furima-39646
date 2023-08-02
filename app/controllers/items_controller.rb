6111ffa2274f866a3a57e2c2d3a22dbb0908c86e
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-08-02 18:17:43 +0900

    createアクションの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,10 +9,16 @@ class ItemsController < ApplicationController
   end
 
   def create
+    if @item = Item.create(item_params)
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:image, :name, :explanation, :category_id, :price, :shipping_fee_status_id, :prefecture_id, :schedule_id, :sales_status_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :price,
+                                 :shipping_fee_status_id, :prefecture_id, :schedule_id, :sales_status_id).merge(user_id: current_user.id)
   end
 end
