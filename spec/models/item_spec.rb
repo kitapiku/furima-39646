2ec6129d9c40b3320aefd5dfbe65c093248d213a
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-08-03 17:06:41 +0900

    商品一覧機能の実装(仮1)

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -80,7 +80,7 @@ RSpec.describe Item, type: :model do
       it '出品者情報が紐づいていなければ出品できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
