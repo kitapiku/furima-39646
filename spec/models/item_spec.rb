51e13fc2a8c5f18062ae0f9c02452693a4e64a9f
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-08-03 14:31:03 +0900

    単体テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -27,28 +27,28 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
-      it 'カテゴリー情報が空だと出品できない' do
-        @item.category_id = ''
+      it 'カテゴリー情報が未選択だと出品できない' do
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it '商品の状態が空だと出品できない' do
-        @item.sales_status_id = ''
+      it '商品の状態が未選択だと出品できない' do
+        @item.sales_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Sales status can't be blank")
       end
-      it '配送料の負担が空だと出品できない' do
-        @item.shipping_fee_status_id = ''
+      it '配送料の負担が未選択だと出品できない' do
+        @item.shipping_fee_status_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee status can't be blank")
       end
-      it '発送元の地域が空だと出品できない' do
-        @item.prefecture_id = ''
+      it '発送元の地域が未選択だと出品できない' do
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
-      it '発送までの日数が空だと出品できない' do
-        @item.schedule_id = ''
+      it '発送までの日数が未選択だと出品できない' do
+        @item.schedule_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Schedule can't be blank")
       end
@@ -71,6 +71,16 @@ RSpec.describe Item, type: :model do
         @item.price = '１０００'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it '価格が小数点だと出品できない' do
+        @item.price = 1000.5
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be an integer')
+      end
+      it '出品者情報が紐づいていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
