9b6c22e89ddce44e222fa4c387b1a885b3ae1ffe
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-08-03 14:30:50 +0900

    商品価格のバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
   validates :explanation, presence: true
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true,
-                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+                    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
   validates :shipping_fee_status_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :schedule_id, numericality: { other_than: 1, message: "can't be blank" }
