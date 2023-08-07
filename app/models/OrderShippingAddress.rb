afaf7427d2a9a1538d4eaf1d6ae9ed6d23ba46e4
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-08-07 14:02:18 +0900

    モデル設定

---
+++ app/models/OrderShippingAddress.rb
@@ -0,0 +1,25 @@
+class OrderShippingAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipalities, :address, :building, :phone_number 
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)'}
+    validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
+    validates :municipalities
+    validates :address
+    validates :phone_number, numericality: { only_integer: true }, length: { in: 10..11 }
+  end
+
+  def purchase
+    order = Order.create(user_id: user_id, item_id: item_id)
+    ShippingAaddress.create(post_code: post_code,
+                            prefecture_id: prefecture_id,
+                            municipalities: municipalities,
+                            address: address,
+                            building: building,
+                            phone_number: phone_number,
+                            order_id: order.id)
+  end
+end
