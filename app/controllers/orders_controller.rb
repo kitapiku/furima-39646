bf86f0f8ef71a27949df5fa14f7c757c7467fba8
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-08-07 14:02:43 +0900

    ordersコントローラー作成

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,26 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @item = Item.find(params[:item_id])
+    @order_shipping_address = OrderShippingAddress.new
+  end
+
+  def create
+    @order_shipping_address = OrderShippingAddress.new(order_shipping_address_params)
+    if @order_shipping_address.valid?
+      @order_shipping_address.purchase
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_shipping_address_params
+    params.require(:order_shipping_address)
+          .permit(:post_code, :prefecture_idd, :municipalities, :address, :building, :phone_number, :order_id)
+          .merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
