6222f35c39663a279a0ceeedf00bfee7c675dd71
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-08-08 14:35:45 +0900

    Rubocopによる修正

--- spec/factories/order_shipping_addresses.rb
+++ spec/factories/order_shipping_addresses.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :order_shipping_address do
-    post_code {'123-4567'}
+    post_code { '123-4567' }
     prefecture_id { rand(2..48) }
-    municipalities {'昆布出市'}
-    address {'豆腐1-1-1'}
-    building {'一本木ヒルズ101号室'}
-    phone_number {'09012345678'}
-    token {"tok_abcdefghijk00000000000000000"}
+    municipalities { '昆布出市' }
+    address { '豆腐1-1-1' }
+    building { '一本木ヒルズ101号室' }
+    phone_number { '09012345678' }
+    token { 'tok_abcdefghijk00000000000000000' }
   end
 end
