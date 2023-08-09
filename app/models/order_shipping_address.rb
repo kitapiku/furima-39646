3925fa871fbfef184ed2b3dae051dfa4dea2a87d
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-08-09 16:45:42 +0900

    エラーメッセージの日本語化の実装

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -6,8 +6,8 @@ class OrderShippingAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフンを含めて入力してください' }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'を入力してください' }
     validates :municipalities
     validates :address
     validates :phone_number, numericality: { only_integer: true }, length: { in: 10..11 }
