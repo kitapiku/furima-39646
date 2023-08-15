b2c8c71488661ac31a23cf23b84789e2a601652a
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-08-15 15:36:39 +0900

    画像プレビュー表示と複数投稿機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,8 +49,8 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :explanation, :category_id, :price,
-                                 :shipping_fee_status_id, :prefecture_id, :schedule_id, :sales_status_id).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :explanation, :category_id, :price, :shipping_fee_status_id,
+                                 :prefecture_id, :schedule_id, :sales_status_id, {images: []}).merge(user_id: current_user.id)
   end
 
   def set_item
