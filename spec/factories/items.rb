6323a4ef8d2f50eec0c714106a610df6e5b17fc9
kitapiku <139326453+kitapiku@users.noreply.github.com>
2023-08-15 17:03:56 +0900

    購入画面の修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -12,7 +12,7 @@ FactoryBot.define do
     association :user
 
     after(:build) do |item|
-      item.image.attach(io: File.open('public/image/img_rajang01_l.png'), filename: 'img_rajang01_l.png')
+      item.images.attach(io: File.open('public/image/img_rajang01_l.png'), filename: 'img_rajang01_l.png')
     end
   end
 end
